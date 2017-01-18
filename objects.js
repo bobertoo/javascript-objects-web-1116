ff5e2be750f741a7d5ae9f9c460a5d213491c8cf
Vinny Martinez <vinny@vinnymartinez.com>
2017-01-18 14:03:37 -0500

    flock of green lights

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'artist' : ['Drake'], 'song' : ['Hotline Bling']}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
